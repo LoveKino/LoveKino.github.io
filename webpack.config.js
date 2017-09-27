ba66ba78abf7822f4029d982ee7e623e5bd00798
loveKino <842914439@qq.com>
2017-09-27 17:54:50 +0800

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,12 @@
 'use strict';
 
-var path = require('path');
-var webpack = require('webpack');
-var WebpackMd5Hash = require('webpack-md5-hash');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 module.exports = {
     entry: {
-        app: ['./src/main.js'],
-        vendor: []
+        app: './lib/index.js'
     },
+
     output: {
-        path: path.resolve(__dirname, 'assets'),
-        filename: '[name].[chunkhash].js'
-    },
-    module: {
-        loaders: [{
-            test: /\.json$/,
-            loader: 'json'
-        }, {
-            test: /\.jsx?$/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015', 'stage-0']
-            }
-        }, {
-            test: /\.less$/,
-            loader: 'style!css!less'
-        }, {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        }, {
-            test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: 'url'
-        }]
-    },
-    plugins: [
-        new WebpackMd5Hash(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor',
-            filename: 'vendor.[chunkhash].js',
-            minChunks: Infinity
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-        new HtmlWebpackPlugin({
-            title: 'ddchen\'s blog',
-            filename: __dirname + '/index.html'
-        }),
-        new ExtractTextPlugin('[name].css')
-    ]
+        path: __dirname + '/asset',
+        filename: '[name].js'
+    }
 };
